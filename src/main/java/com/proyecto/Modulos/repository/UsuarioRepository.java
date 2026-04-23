c1c3774519b281f2d46e949d855a89c8ab92fa08
Royser-S <royserfonce@gmail.com>
2026-04-23 13:40:16 -0500

    Fix: Sincronización automática de usuarios para barberos y clientes

--- src/main/java/com/proyecto/Modulos/repository/UsuarioRepository.java
+++ src/main/java/com/proyecto/Modulos/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.proyecto.Modulos.repository;
 
+import com.proyecto.Modulos.entity.Barbero;
+import com.proyecto.Modulos.entity.Cliente;
 import com.proyecto.Modulos.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +11,7 @@ import java.util.Optional;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     Optional<Usuario> findByUsername(String username);
+    Optional<Usuario> findByBarbero(Barbero barbero);
+    Optional<Usuario> findByCliente(Cliente cliente);
 }
+
